a3d2291b1668eaafe1a6d3f1bd9ab4f24c673469
violaine-drt <viodarrent@gmail.com>
2024-03-10 16:58:56 +0100

    added button from homepage to gribouillis

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,18 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
-export class LandingPageComponent {
-  constructor() {}
+export class LandingPageComponent implements OnInit{
+  constructor(private router : Router) {}
   
   ngOnInit(): void {
   }
 
   onContinue(): void {
-    
+    this.router.navigateByUrl("gribouillis")
   }
 
 }
